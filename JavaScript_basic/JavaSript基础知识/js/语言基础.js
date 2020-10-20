c4f694d781e4425719bbbe73654261fb9b3aaae8
meakle <meakle@qq.com>
2020-10-20 15:47:58 +0800

    add 4-语言基础 javaScript

---
+++ JavaScript_basic/JavaSript基础知识/js/语言基础.js
@@ -0,0 +1,11 @@
+console.log(name); //matter
+
+var name = 'nicholas';
+console.log(name); //nicholas
+
+if (true) {
+    var name = 'matter';
+    console.log(name); //matter
+}
+
+console.log(name); //matte
